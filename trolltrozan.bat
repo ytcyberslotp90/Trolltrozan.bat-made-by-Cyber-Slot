42a0629132d255eeda556ce0de097d7b0a9e43b9
ytcyberslotp90 <krishnendumaji3633@gmail.com>
2025-05-27 16:49:47 +0530

    Add files via upload

---
+++ trolltrozan.bat
@@ -0,0 +1,71 @@
+@echo off                                                                                                                                                                                     
+echo ooooooooooooo                    oooo  oooo      .                                                       
+echo 8'   888   `8                    `888  `888    .o8                                                       
+echo      888      oooo d8b  .ooooo.   888   888  .o888oo oooo d8b  .ooooo.    oooooooo  .oooo.   ooo. .oo.   
+echo      888      `888""8P d88' `88b  888   888    888   `888""8P d88' `88b  d'""7d8P  `P  )88b  `888P"Y88b  
+echo      888       888     888   888  888   888    888    888     888   888    .d8P'    .oP"888   888   888  
+echo      888       888     888   888  888   888    888 .  888     888   888  .d8P'  .P d8(  888   888   888  
+echo     o888o     d888b    `Y8bod8P' o888o o888o   "888" d888b    `Y8bod8P' d8888888P  `Y888""8o o888o o888o 
+                                                                                                         
+                                                                                                         
+                                                                                                         
+
+title trolltrozan 2.0 by Cyber Slot
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "tsspeech.Vbs"
+set "text=attention please i am trolltrozan made by cyber slot it can crash your PC simply type no to stop the destruction or type yes for destruction..."
+echo speech.speak "%text%" >> "tsspeech.vbs"
+start tsspeech.vbs
+color a
+
+echo warning it can crash your PC simply type no to stop the destruction or type yes for destruction...
+set /p opn=enter yes/no:
+if "%opn%"=="no" exit
+echo do you still want to continue ?
+set /p opn=enter yes/no:
+if "%opn%"=="no" exit 
+pause
+echo MsgBox "see your keyboard",64, "TrollTrozan" >VbsMsgboxTwSykqwertyuiopasdfghjklzxcvbnmqazwsxedcrfvtgbyhnujm.VBS
+START VbsMsgboxTwSykqwertyuiopasdfghjklzxcvbnmqazwsxedcrfvtgbyhnujm.VBS
+START https://www.google.com/search?q=how+to+download+Salinwin.exe
+START https://www.google.com/search?q=how+to+download+Memz.exe
+START https://www.youtube.com/channel/UCON2q5tin1NHZ0J6G7Yi_Kw
+START https://www.google.com/search?q=how+to+download+Sulfoid.exe
+START https://www.google.com/search?q=how+to+download+Salinwin.exe
+START https://www.google.com/search?q=how+to+download+YouAreAnIdiot
+START https://www.google.com/search?q=how+to+download+kittyhacker.exe
+START https://www.google.com/search?q=pankoza2
+START https://www.google.com/search?q=how+to+download+Scorpion.exe
+START https://www.google.com/search?q=the+annoying+site
+
+
+
+set "vbsFile=%~dp0trolltrozanwork.vbs"
+
+
+echo Dim speech >> "%vbsFile%"
+echo Set speech = CreateObject("SAPI.SpVoice") >> "%vbsFile%"
+echo speech.Speak "O K sit back and see what can i do to your p c" >> "%vbsFile%"
+echo Set WshShell = WScript.CreateObject("WScript.Shell") >> "%vbsFile%"
+echo WshShell.Run "notepad.exe", 9 >> "%vbsFile%"
+echo do >> "%vbsFile%"
+echo Set objShell = CreateObject("WScript.Shell") >> "%vbsFile%"
+echo objShell.Run "powershell -c [System.Media.SystemSounds]::Exclamation.Play()", 0, True >> "%vbsFile%"
+echo Set objShell = CreateObject("WScript.Shell") >> "%vbsFile%"
+echo objShell.Run "powershell -c [System.Media.SystemSounds]::Hand.Play()", 0, True >> "%vbsFile%"
+echo Set objShell = CreateObject("WScript.Shell") >> "%vbsFile%"
+echo objShell.SendKeys "%%{TAB}" >> "%vbsFile%"
+echo objShell.SendKeys "^{ESC}" >> "%vbsFile%"
+echo WScript.Sleep 10 >> "%vbsFile%"
+echo Set wshShell =wscript.CreateObject("WScript.Shell") >> "%vbsFile%"
+echo wscript.sleep 100 >> "%vbsFile%"
+echo wshshell.sendkeys "{CAPSLOCK}" >> "%vbsFile%"
+echo wshshell.sendkeys "{NUMLOCK}" >> "%vbsFile%"
+echo wshshell.sendkeys "{SCROLLLOCK}" >> "%vbsFile%"
+echo loop >> "%vbsFile%"
+
+start trolltrozanwork.vbs
+del VbsMsgboxTwSykqwertyuiopasdfghjklzxcvbnmqazwsxedcrfvtgbyhnujm.VBS
+del tsspeech.Vbs
+del trolltrozanwork.vbs
+exit
+
